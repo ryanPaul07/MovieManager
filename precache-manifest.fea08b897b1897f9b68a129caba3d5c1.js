a921c5e18c84b860f9493dd779a57a0499ad41a6
Ryan Paul <rpaul06@uoguelph.ca>
2020-09-15 13:46:09 -0400

    Updates

---
+++ precache-manifest.fea08b897b1897f9b68a129caba3d5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e720b75b03258b3995a9f14584aab1de",
+    "url": "/MovieManager/index.html"
+  },
+  {
+    "revision": "3df6d796e6859d9d1cd0",
+    "url": "/MovieManager/static/css/main.f72edae1.chunk.css"
+  },
+  {
+    "revision": "90cbca6d25d308074540",
+    "url": "/MovieManager/static/js/2.35352389.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MovieManager/static/js/2.35352389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df6d796e6859d9d1cd0",
+    "url": "/MovieManager/static/js/main.1d6a05e6.chunk.js"
+  },
+  {
+    "revision": "40569f74ab971140393a",
+    "url": "/MovieManager/static/js/runtime-main.4be11a47.js"
+  }
+]);
